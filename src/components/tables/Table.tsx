3687b578a4b5f8aa1340772bbec738e66c3d9886
Nick Florin <nickmflorin@gmail.com>
2024-02-13 16:01:39 -0500

    Adding better styling to table

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -1,10 +1,46 @@
 "use client";
-import { DataTable } from "mantine-datatable";
+import dynamic from "next/dynamic";
 
-import { type TableProps } from "./types";
+import clsx from "clsx";
+import { type DataTableProps } from "mantine-datatable";
 
-export const Table = <T extends { id: string }>({ data, columns }: TableProps<T>): JSX.Element => (
-  <DataTable columns={columns} records={data} />
-);
+import { Loading } from "~/components/views/Loading";
+
+import { type TableProps, TableSizes } from "./types";
+
+const MantineDataTable = dynamic(() => import("mantine-datatable").then(i => i.DataTable)) as {
+  <T>(props: DataTableProps<T>): JSX.Element;
+};
+
+export const Table = <T extends { id: string }>({
+  size = TableSizes.SM,
+  columns,
+  data,
+  className,
+  loading,
+  ...props
+}: TableProps<T>) => {
+  /* Mantine's <DataTable /> component defines the props as a set of base props intersected with a
+     bunch of supplementary props, such as 'DataTableEmptyStateProps', each of which is a union
+     type.  This introduces typing issues when adding our own props into the fold, because the
+     intersection of our props with their props does not distribute our props over the union types.
+     The only ways around this are to define the props for this component in an extremely
+     complicated manner, or simply coerce the props as shown below. */
+  const rootProps: DataTableProps<T> = {
+    highlightOnHover: false,
+    withBorder: false,
+    height: "100%",
+    className: clsx("data-table", `data-table--size-${size}`, className),
+    customLoader: <Loading overlay={true} loading={true} />,
+    // TODO: Revisit this later.
+    emptyState: <></>,
+    ...props,
+    fetching: loading,
+    records: data,
+    columns,
+  } as DataTableProps<T>;
+
+  return <MantineDataTable<T> {...rootProps} />;
+};
 
 export default Table;
--- src/components/tables/types.ts
+++ src/components/tables/types.ts
@@ -1,6 +1,14 @@
 import { type DataTableColumn } from "mantine-datatable";
 
-export interface TableProps<T extends { id: string }> {
+import { enumeratedLiterals, type EnumeratedLiteralsType } from "~/lib/literals";
+import { type ComponentProps } from "~/components/types";
+
+export const TableSizes = enumeratedLiterals(["sm", "md", "lg"] as const, {});
+export type TableSize = EnumeratedLiteralsType<typeof TableSizes>;
+
+export interface TableProps<T extends { id: string }> extends ComponentProps {
   readonly data: T[];
   readonly columns: DataTableColumn<T>[];
+  readonly size?: TableSize;
+  readonly loading?: boolean;
 }
