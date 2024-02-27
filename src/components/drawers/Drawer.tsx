790f2fde221d36c6040b8c8c1c8fb63e01ba59cd
Nick Florin <nickmflorin@gmail.com>
2024-02-26 20:11:26 -0500

    Fixing overflow bug

--- src/components/drawers/Drawer.tsx
+++ src/components/drawers/Drawer.tsx
@@ -1,19 +1,23 @@
 import dynamic from "next/dynamic";
 import { type ReactNode } from "react";
 
+import clsx from "clsx";
+
+import { type ComponentProps } from "~/components/types";
+
 import { DrawerPortal } from "./DrawerPortal";
 
 const DrawerCloseButton = dynamic(() => import("~/components/buttons/DrawerCloseButton"));
 
-export interface DrawerProps {
+export interface DrawerProps extends ComponentProps {
   readonly open: boolean;
   readonly children: ReactNode;
   readonly onClose?: () => void;
 }
 
-export const Drawer = ({ children, open, onClose }: DrawerProps): JSX.Element => (
+export const Drawer = ({ children, open, onClose, ...props }: DrawerProps): JSX.Element => (
   <DrawerPortal open={open}>
-    <div className="drawer">
+    <div {...props} className={clsx("drawer", props.className)}>
       {children}
       {onClose && <DrawerCloseButton onClick={onClose} />}
     </div>
