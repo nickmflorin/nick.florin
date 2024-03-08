253c9654e65877b2de685cef74fbc946aceb2c62
Nick Florin <nickmflorin@gmail.com>
2024-03-08 12:17:27 -0500

    Fixing resume page timelines

--- src/components/timelines/EducationTimeline/index.tsx
+++ src/components/timelines/EducationTimeline/index.tsx
@@ -1,9 +1,17 @@
+import dynamic from "next/dynamic";
+
 import { getEducations } from "~/actions/fetches/get-educations";
+import { TimelineIcon } from "~/components/icons/TimelineIcon";
 import { type ComponentProps } from "~/components/types";
+import { Loading } from "~/components/views/Loading";
 
 import { CommitTimeline } from "../CommitTimeline";
 
 import { EducationTile } from "./EducationTile";
+
+const TimelineItem = dynamic(() => import("@mantine/core").then(mod => mod.TimelineItem), {
+  loading: () => <Loading loading={true} />,
+});
 
 export type EducationTimelineProps = ComponentProps;
 
@@ -12,7 +20,9 @@ export const EducationTimeline = async (props: EducationTimelineProps): Promise<
   return (
     <CommitTimeline {...props}>
       {educations.map(education => (
-        <EducationTile key={education.id} education={education} />
+        <TimelineItem key={education.id} bullet={<TimelineIcon />}>
+          <EducationTile education={education} />
+        </TimelineItem>
       ))}
     </CommitTimeline>
   );
--- src/components/timelines/ExperienceTimeline/index.tsx
+++ src/components/timelines/ExperienceTimeline/index.tsx
@@ -1,9 +1,17 @@
+import dynamic from "next/dynamic";
+
 import { getExperiences } from "~/actions/fetches/get-experiences";
+import { TimelineIcon } from "~/components/icons/TimelineIcon";
 import { type ComponentProps } from "~/components/types";
+import { Loading } from "~/components/views/Loading";
 
 import { CommitTimeline } from "../CommitTimeline";
 
 import { ExperienceTile } from "./ExperienceTile";
+
+const TimelineItem = dynamic(() => import("@mantine/core").then(mod => mod.TimelineItem), {
+  loading: () => <Loading loading={true} />,
+});
 
 export type ExperienceTimelineProps = ComponentProps;
 
@@ -12,7 +20,9 @@ export const ExperienceTimeline = async (props: ExperienceTimelineProps): Promis
   return (
     <CommitTimeline {...props}>
       {experiences.map(experience => (
-        <ExperienceTile key={experience.id} experience={experience} />
+        <TimelineItem key={experience.id} bullet={<TimelineIcon />}>
+          <ExperienceTile experience={experience} />
+        </TimelineItem>
       ))}
     </CommitTimeline>
   );
