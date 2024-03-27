03184d621efcdf98ede5d067d8a8712cff1a6c8e
Nick Florin <nickmflorin@gmail.com>
2024-03-26 20:24:43 -0400

    Try this

--- src/app/api/skills/[id]/route.ts
+++ src/app/api/skills/[id]/route.ts
@@ -5,12 +5,12 @@ import { type ApiSkill } from "~/prisma/model";
 import { getSkill } from "~/actions/fetches/get-skill";
 import { ClientResponse, ApiClientGlobalError } from "~/http";
 
-export async function generateStaticParams() {
-  const skills = await prisma.skill.findMany();
-  return skills.map(skill => ({
-    id: skill.id,
-  }));
-}
+/* export async function generateStaticParams() {
+     const skills = await prisma.skill.findMany();
+     return skills.map(skill => ({
+       id: skill.id,
+     }));
+   } */
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const skill = await getSkill(params.id, { visibility: "public" });
