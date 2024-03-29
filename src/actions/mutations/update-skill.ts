14c09350b979680ea89bdf6bf2a2405cb5cbd0ba
Nick Florin <nickmflorin@gmail.com>
2024-03-28 21:17:18 -0400

    Fix bug updating with empty array from admin

--- src/actions/mutations/update-skill.ts
+++ src/actions/mutations/update-skill.ts
@@ -7,12 +7,109 @@ import { getAuthAdminUser } from "~/application/auth";
 import { objIsEmpty } from "~/lib";
 import { slugify } from "~/lib/formatters";
 import { prisma } from "~/prisma/client";
-import { type Skill, type Experience, type Education } from "~/prisma/model";
+import {
+  type Skill,
+  type Experience,
+  type Education,
+  type PrismaClient,
+  type User,
+} from "~/prisma/model";
 import { ApiClientFormError, type ApiClientFieldErrors } from "~/http";
 
 import { SkillSchema } from "../schemas";
 
 const UpdateSkillSchema = SkillSchema.partial();
+
+type Transaction = Omit<
+  PrismaClient,
+  "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends"
+>;
+
+const syncExperiences = async (
+  tx: Transaction,
+  { skill, experiences, user }: { skill: Skill; user: User; experiences?: Experience[] },
+) => {
+  if (experiences) {
+    const relationships = await tx.experienceOnSkills.findMany({
+      where: { skillId: skill.id },
+    });
+    /* We need to remove the relationship between the skill and the experience if there is an
+       existing relationship associated with the experience but the experience's ID is not included
+       in the API request. */
+    const toRemove = relationships.filter(
+      r => !experiences.map(e => e.id).includes(r.experienceId),
+    );
+    if (toRemove.length !== 0) {
+      await Promise.all(
+        toRemove.map(relationship =>
+          tx.experienceOnSkills.delete({
+            where: {
+              skillId_experienceId: {
+                skillId: relationship.skillId,
+                experienceId: relationship.experienceId,
+              },
+            },
+          }),
+        ),
+      );
+    }
+    /* We need to add relationships between an experience and the skill if the experience's ID is
+       included in the API request and there is not an existing relationship between that
+       experience and the skill. */
+    const toAdd = experiences.filter(e => !relationships.some(r => r.experienceId === e.id));
+    if (toAdd.length !== 0) {
+      await tx.experienceOnSkills.createMany({
+        data: toAdd.map(e => ({
+          skillId: skill.id,
+          experienceId: e.id,
+          assignedById: user.id,
+        })),
+      });
+    }
+  }
+};
+
+const syncEducations = async (
+  tx: Transaction,
+  { skill, educations, user }: { skill: Skill; user: User; educations?: Education[] },
+) => {
+  if (educations) {
+    const relationships = await tx.educationOnSkills.findMany({
+      where: { skillId: skill.id },
+    });
+    /* We need to remove the relationship between the skill and the experience if there is an
+       existing relationship associated with the experience but the experience's ID is not included
+       in the API request. */
+    const toRemove = relationships.filter(r => !educations.map(e => e.id).includes(r.educationId));
+    if (toRemove.length !== 0) {
+      await Promise.all(
+        toRemove.map(relationship =>
+          tx.educationOnSkills.delete({
+            where: {
+              skillId_educationId: {
+                skillId: relationship.skillId,
+                educationId: relationship.educationId,
+              },
+            },
+          }),
+        ),
+      );
+    }
+    /* We need to add relationships between an experience and the skill if the experience's ID is
+       included in the API request and there is not an existing relationship between that
+       experience and the skill. */
+    const toAdd = educations.filter(e => !relationships.some(r => r.educationId === e.id));
+    if (toAdd.length !== 0) {
+      await tx.educationOnSkills.createMany({
+        data: toAdd.map(e => ({
+          skillId: skill.id,
+          educationId: e.id,
+          assignedById: user.id,
+        })),
+      });
+    }
+  }
+};
 
 export const updateSkill = async (
   id: string,
@@ -43,8 +140,8 @@ export const updateSkill = async (
     }
 
     let fieldErrors: ApiClientFieldErrors = {};
-    let experiences: Experience[] = [];
-    let educations: Education[] = [];
+    let experiences: Experience[] | undefined = undefined;
+    let educations: Education[] | undefined = undefined;
 
     if (_experiences !== undefined) {
       experiences = await tx.experience.findMany({ where: { id: { in: _experiences } } });
@@ -74,84 +171,9 @@ export const updateSkill = async (
       throw ApiClientFormError.BadRequest(fieldErrors);
     }
 
-    if (experiences.length) {
-      const currentRelationships = await tx.experienceOnSkills.findMany({
-        where: { skillId: skill.id },
-      });
-      const ids = experiences.map(e => e.id);
+    await syncExperiences(tx, { experiences, skill, user });
+    await syncEducations(tx, { educations, skill, user });
 
-      /* We need to remove the relationship between the skill and the experience if there is an
-         existing relationship associated with the experience but the experience's ID is not
-         included in the API request. */
-      const toRemove = currentRelationships.filter(r => !ids.includes(r.experienceId));
-      if (toRemove.length !== 0) {
-        await Promise.all(
-          toRemove.map(relationship =>
-            tx.experienceOnSkills.delete({
-              where: {
-                skillId_experienceId: {
-                  skillId: relationship.skillId,
-                  experienceId: relationship.experienceId,
-                },
-              },
-            }),
-          ),
-        );
-      }
-      /* We need to add relationships between an experience and the skill if the experience's ID is
-         included in the API request and there is not an existing relationship between that
-         experience and the skill. */
-      const toAdd = experiences.filter(
-        e => !currentRelationships.some(r => r.experienceId === e.id),
-      );
-      if (toAdd.length !== 0) {
-        await tx.experienceOnSkills.createMany({
-          data: toAdd.map(e => ({
-            skillId: skill.id,
-            experienceId: e.id,
-            assignedById: user.id,
-          })),
-        });
-      }
-    }
-    if (educations.length) {
-      const currentRelationships = await tx.educationOnSkills.findMany({
-        where: { skillId: skill.id },
-      });
-      const ids = educations.map(e => e.id);
-
-      /* We need to remove the relationship between the skill and the education if there is an
-         existing relationship associated with the education but the education's ID is not
-         included in the API request. */
-      const toRemove = currentRelationships.filter(r => !ids.includes(r.educationId));
-      if (toRemove.length !== 0) {
-        await Promise.all(
-          toRemove.map(relationship =>
-            tx.educationOnSkills.delete({
-              where: {
-                skillId_educationId: {
-                  skillId: relationship.skillId,
-                  educationId: relationship.educationId,
-                },
-              },
-            }),
-          ),
-        );
-      }
-      /* We need to add relationships between an education and the skill if the education's ID is
-         included in the API request and there is not an existing relationship between that
-         education and the skill. */
-      const toAdd = educations.filter(e => !currentRelationships.some(r => r.educationId === e.id));
-      if (toAdd.length !== 0) {
-        await tx.educationOnSkills.createMany({
-          data: toAdd.map(e => ({
-            skillId: skill.id,
-            educationId: e.id,
-            assignedById: user.id,
-          })),
-        });
-      }
-    }
     return skill;
   });
   revalidatePath("/admin/skills", "page");
