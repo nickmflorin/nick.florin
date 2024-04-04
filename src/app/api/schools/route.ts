41ab60728bb025aba9532d81dcdfa01ff732a5c7
Nick Florin <nickmflorin@gmail.com>
2024-04-04 19:02:00 -0400

    Squashing bug

--- src/app/api/schools/route.ts
+++ src/app/api/schools/route.ts
@@ -1,9 +1,15 @@
+import { type NextRequest } from "next/server";
+
 import { getAuthAdminUser } from "~/application/auth";
+import { type SchoolIncludes } from "~/prisma/model";
 import { getSchools } from "~/actions/fetches/schools";
 import { ClientResponse } from "~/api";
+import { parseInclusion } from "~/api/query";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   await getAuthAdminUser();
-  const schools = await getSchools();
+  const schools = await getSchools({
+    includes: parseInclusion(request, ["educations"] as const) as SchoolIncludes,
+  });
   return ClientResponse.OK(schools).response;
 }
