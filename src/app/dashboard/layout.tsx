8eb1047fd1cb0837471291da28426ec9061818b6
Nick Florin <nickmflorin@gmail.com>
2024-05-06 12:38:34 -0400

    Improvements to responsive design of dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,10 +5,30 @@ import clsx from "clsx";
 
 import type { LinkComponent } from "~/components/buttons";
 import { Module } from "~/components/modules/generic";
+import { type ComponentProps } from "~/components/types";
 
 const Link = dynamic(() =>
   import("~/components/buttons/generic/Link").then(mod => mod.Link),
 ) as LinkComponent;
+
+interface ColumnProps extends ComponentProps {
+  readonly children: ReactNode;
+}
+
+const Column = ({ children, ...props }: ColumnProps): JSX.Element => (
+  <div
+    {...props}
+    className={clsx(
+      "flex flex-col gap-[15px] min-w-[320px]",
+      "xl:max-w-[520px]",
+      "max-xl:w-[50%] max-xl:max-w-[50%]",
+      "max-md:w-full max-md:max-w-full",
+      props.className,
+    )}
+  >
+    {children}
+  </div>
+);
 
 export interface DashboardPageProps {
   readonly experiences: ReactNode;
@@ -28,43 +48,41 @@ export default async function DashboardPage({
   return (
     <div className={clsx("max-h-full", "overflow-y-auto pr-[12px]")}>
       <div className={clsx("flex gap-[15px]", "xl:flex-row", "max-xl:flex-col")}>
-        <Module>{chart}</Module>
-        <div className={clsx("flex gap-[15px]", "md:flex-row", "xl:h-full", "max-md:flex-col")}>
-          <Module
-            className={clsx(
-              "xl:max-w-[520px] xl:min-w-[400px]",
-              "max-xl:w-[50%] max-xl:max-w-[50%]",
-              "max-md:w-full max-md:max-w-full",
-            )}
-          >
-            <Module.Header
-              actions={[
-                <Link
-                  key="0"
-                  as="link"
-                  href="/resume/experience"
-                  fontSize="xs"
-                  flex
-                  fontWeight="medium"
-                >
-                  View All
-                </Link>,
-              ]}
-            >
-              Recent Experiences
-            </Module.Header>
-            <Module.Content className="flex flex-col gap-[12px] xl:overflow-y-auto xl:pr-[16px]">
-              {experiences}
-            </Module.Content>
-          </Module>
-          <div
-            className={clsx(
-              "flex flex-col gap-[15px]",
-              "xl:max-w-[520px] xl:min-w-[400px]",
-              "max-xl:w-[50%] max-xl:max-w-[50%]",
-              "max-md:w-full max-md:max-w-full",
-            )}
-          >
+        {/* The 655px comes from 2(320px for each column) + 15px for gap = 655px. */}
+        <Module className={clsx("md:max-lg:min-w-[655px] lg:min-w-[320px]")}>{chart}</Module>
+        <div className={clsx("flex gap-[15px]", "md:flex-row", "max-md:flex-col")}>
+          <Column className="md:flex-1">
+            <Module className="xl:overflow-y-hidden grow">
+              <Module.Header
+                actions={[
+                  <Link
+                    key="0"
+                    as="link"
+                    href="/resume/experience"
+                    fontSize="xs"
+                    flex
+                    fontWeight="medium"
+                  >
+                    View All
+                  </Link>,
+                ]}
+              >
+                Recent Experiences
+              </Module.Header>
+              <Module.Content className="flex flex-col gap-[12px] xl:overflow-y-auto xl:pr-[16px]">
+                {experiences}
+              </Module.Content>
+            </Module>
+            <Module>
+              <Module.Header>Projects</Module.Header>
+              <Module.Content
+                className={clsx("flex flex-col gap-[12px] xl:overflow-y-auto xl:grow xl:pr-[16px]")}
+              >
+                {projects}
+              </Module.Content>
+            </Module>
+          </Column>
+          <Column className="md:flex-1">
             <Module className="xl:overflow-y-hidden">
               <Module.Header
                 actions={[
@@ -91,21 +109,13 @@ export default async function DashboardPage({
                 {educations}
               </Module.Content>
             </Module>
-            <Module className="xl:overflow-y-hidden">
+            <Module className="xl:overflow-y-hidden grow">
               <Module.Header>Repositories</Module.Header>
               <Module.Content className={clsx("xl:overflow-y-auto xl:grow xl:pr-[16px]")}>
                 {repositories}
               </Module.Content>
             </Module>
-            <Module>
-              <Module.Header>Projects</Module.Header>
-              <Module.Content
-                className={clsx("flex flex-col gap-[12px] xl:overflow-y-auto xl:grow xl:pr-[16px]")}
-              >
-                {projects}
-              </Module.Content>
-            </Module>
-          </div>
+          </Column>
         </div>
       </div>
     </div>
