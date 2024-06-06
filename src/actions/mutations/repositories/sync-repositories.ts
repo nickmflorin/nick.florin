6cdade034f9313dd271aa26dc64ff514dbebdc35
Nick Florin <nickmflorin@gmail.com>
2024-06-06 09:34:26 -0400

    Fix script bug

--- src/actions/mutations/repositories/sync-repositories.ts
+++ src/actions/mutations/repositories/sync-repositories.ts
@@ -1,8 +1,11 @@
 "use server";
 import { getAuthedUser } from "~/application/auth/server";
+import { prisma } from "~/prisma/client";
 import { githubClient } from "~/integrations/github";
 
 export const syncRepositories = async () => {
   const { user } = await getAuthedUser();
-  await githubClient.syncRepositories({ user });
+  await prisma.$transaction(async tx => {
+    await githubClient.syncRepositories({ tx, user });
+  });
 };
--- src/integrations/github/client.ts
+++ src/integrations/github/client.ts
@@ -1,4 +1,4 @@
-import { prisma, type Transaction } from "~/prisma/client";
+import { type Transaction } from "~/prisma/client";
 import type { User } from "~/prisma/model";
 import { environment } from "~/environment";
 
@@ -6,28 +6,7 @@ import * as types from "./types";
 
 interface SyncRepositoriesParams {
   readonly user: User;
-  readonly tx?: Transaction;
-}
-
-type TransactionCallback<T> = (tx: Transaction) => Promise<T>;
-
-async function withinTransaction<T>(fn: TransactionCallback<T>): Promise<T>;
-
-async function withinTransaction<T>(
-  tx: Transaction | undefined,
-  fn: TransactionCallback<T>,
-): Promise<T>;
-
-async function withinTransaction<T>(
-  tx: Transaction | undefined | TransactionCallback<T>,
-  fn?: TransactionCallback<T>,
-): Promise<T> {
-  if (typeof tx === "function") {
-    return await prisma.$transaction(async t => tx(t));
-  } else if (fn !== undefined && tx !== undefined) {
-    return fn(tx);
-  }
-  throw new Error("Invalid method implementation!");
+  readonly tx: Transaction;
 }
 
 export class GithubClient<U extends string> {
@@ -43,39 +22,36 @@ export class GithubClient<U extends string> {
     return await response.json();
   }
 
-  async syncRepositories({ tx: _tx, user }: SyncRepositoriesParams): Promise<void> {
+  async syncRepositories({ tx, user }: SyncRepositoriesParams): Promise<void> {
     const githubRepos = await this.fetchRepositories();
-
-    withinTransaction(_tx, async tx => {
-      for (const repo of githubRepos) {
-        const existing = await tx.repository.findUnique({ where: { slug: repo.name } });
-        if (existing) {
-          if (existing.description === undefined || existing.startDate === undefined) {
-            await tx.repository.update({
-              where: { id: existing.id },
-              data: {
-                description: existing.description ?? repo.description,
-                startDate: existing.startDate ?? new Date(repo.created_at),
-                createdBy: { connect: { id: user.id } },
-                updatedBy: { connect: { id: user.id } },
-              },
-            });
-          }
-        } else {
-          await tx.repository.create({
+    for (const repo of githubRepos) {
+      const existing = await tx.repository.findUnique({ where: { slug: repo.name } });
+      if (existing) {
+        if (existing.description === undefined || existing.startDate === undefined) {
+          await tx.repository.update({
+            where: { id: existing.id },
             data: {
-              slug: repo.name,
-              description: repo.description,
-              startDate: new Date(repo.created_at),
+              description: existing.description ?? repo.description,
+              startDate: existing.startDate ?? new Date(repo.created_at),
               createdBy: { connect: { id: user.id } },
               updatedBy: { connect: { id: user.id } },
-              visible: false,
-              highlighted: false,
             },
           });
         }
+      } else {
+        await tx.repository.create({
+          data: {
+            slug: repo.name,
+            description: repo.description,
+            startDate: new Date(repo.created_at),
+            createdBy: { connect: { id: user.id } },
+            updatedBy: { connect: { id: user.id } },
+            visible: false,
+            highlighted: false,
+          },
+        });
       }
-    });
+    }
   }
 }
 
