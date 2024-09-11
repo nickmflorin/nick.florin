0dd354552ca4bd10998002c6790093a64a264bea
Nick Florin <nickmflorin@gmail.com>
2024-09-11 16:17:57 -0400

    fix small tab space bug

--- src/components/buttons/TabButton.tsx
+++ src/components/buttons/TabButton.tsx
@@ -24,13 +24,12 @@ export const TabButton = <F extends ButtonForm>({
       "rounded-none rounded-t-md relative top-[2px]",
       "border-b-[2px] text-gray-800",
       "hover:bg-neutral-100",
-      "max-sm:rounded-md top-0",
       {
         "border-transparent hover:border-gray-300": !(isActive || isPending),
         "border-blue-700": isActive || isPending,
         "max-sm:bg-neutral-200": isActive || isPending,
       },
-      "max-sm:border-none",
+      "max-sm:border-none max-sm:rounded-md max-sm:top-0",
       props.className,
     )}
     fontWeight="regular"
