ab22619a530a72bb236d3c054090ff882d16ef12
Nick Florin <nickmflorin@gmail.com>
2024-09-13 14:44:06 -0400

    W

--- src/components/layout/LayoutNavigation.tsx
+++ src/components/layout/LayoutNavigation.tsx
@@ -1,4 +1,8 @@
+import dynamic from "next/dynamic";
+
 import { useScreenSizes } from "~/hooks/use-screen-sizes";
+
+const LayoutMenu = dynamic(() => import("./LayoutMenu"), { ssr: false });
 
 export const LayoutNavigation = () => {
   const { isLessThanOrEqualTo } = useScreenSizes({});
