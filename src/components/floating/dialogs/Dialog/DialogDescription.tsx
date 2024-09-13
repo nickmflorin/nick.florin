5a4cdaf562c5c96d38240cc92681a701830f1e46
Nick Florin <nickmflorin@gmail.com>
2024-09-13 15:47:40 -0400

    Fix ts error

--- src/components/floating/dialogs/Dialog/DialogDescription.tsx
+++ src/components/floating/dialogs/Dialog/DialogDescription.tsx
@@ -1,7 +1,6 @@
-"use client";
 import { Description, type DescriptionProps } from "~/components/typography/Description";
 
-export interface DialogDescriptionProps extends DescriptionProps {}
+export interface DialogDescriptionProps extends DescriptionProps<"div"> {}
 
 export const DialogDescription = ({ children, ...props }: DialogDescriptionProps) => (
   <Description {...props}>{children}</Description>
