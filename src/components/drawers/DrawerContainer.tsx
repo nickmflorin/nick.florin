3777df3a2afd89432be5750b3fcd86fd4741037e
Nick Florin <nickmflorin@gmail.com>
2024-09-15 11:53:59 -0400

    Fix mobile drawer bug

--- src/components/drawers/DrawerContainer.tsx
+++ src/components/drawers/DrawerContainer.tsx
@@ -1,26 +1,23 @@
 import { type ReactNode } from "react";
 
-import {
-  type ComponentProps,
-  type QuantitativeSize,
-  classNames,
-  sizeToString,
-} from "~/components/types";
+import { type ComponentProps, type QuantitativeSize, classNames } from "~/components/types";
 
 export interface DrawerContainerProps extends ComponentProps {
   readonly children: ReactNode;
   readonly width: QuantitativeSize<"px">;
 }
 
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 export const DrawerContainer = ({ children, width, ...props }: DrawerContainerProps) => (
   <div
     {...props}
     className={classNames("drawer-container", props.className)}
     style={{
       ...props.style,
-      width: sizeToString(width, "px"),
-      maxWidth: sizeToString(width, "px"),
-      minWidth: sizeToString(width, "px"),
+      // This messes with the full size view on mobile - we will revisit later.
+      /* width: sizeToString(width, "px"),
+         maxWidth: sizeToString(width, "px"),
+         minWidth: sizeToString(width, "px"), */
     }}
   >
     {children}
